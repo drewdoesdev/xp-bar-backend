76a7a525cbd9ace5bd749ae856076dcb6dcbe02f
drewdoesdev <ataylor2247@gmail.com>
2020-11-17 07:50:48 -0600

    Check added on user api

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const keys = require("../../config/keys");
+const keys = process.env.KEY || require("../../config/keys");
 var ObjectId = require('mongodb').ObjectID;
 
 // Load input validation
